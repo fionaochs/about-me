876d21198042c9bc11ac2cd6eb0877d6d3459c6f
Fiona Ochs <fiona.oaks@gmail.com>
2020-01-14 10:43:46 -0800

    more updates to app.js

--- app.js
+++ app.js
@@ -7,18 +7,34 @@ quizButton.addEventListener('click', () => {
     console.log("hit button");
 
     let name = prompt('What\'s your name?');
-    if (name === null || name ===""){
+    if (name === null || name === ""){
         alert ('You must enter a name!')
-       let name = prompt('What\'s your name?');
+        name = prompt('What\'s your name?');
     }
     const confirmation = confirm(`Hello ${name}, would you like to take a quiz about Fiona?`)
 
     if (confirmation === false) return;
-
-    const questionOne = prompt('Do I like to travel?'); //answer yes
-    const questionTwo = prompt('Am I a fan of live music?'); //answer yes
-    const questionThree = prompt('Do I enjoy baking?'); //answer yes
-    const questionFour = prompt('Do I play musical instruments?'); //answer no
+    
+    let questionOne = prompt('Do I like to travel?'); //answer yes
+    if (questionOne === null || questionOne === ""){
+        alert ('Enter an answer')
+        let questionOne = prompt('Do I like to travel?');
+    }
+    let questionTwo = prompt('Am I a fan of live music?'); //answer yes
+    if (questionTwo === null || questionTwo === ""){
+        alert ('Enter an answer')
+        let questionTwo = prompt('Am I a fan of live music?');
+    }
+    let questionThree = prompt('Do I enjoy baking?'); //answer yes
+    if (questionThree === null || questionThree === ""){
+        alert ('Enter an answer')
+        let questionThree = prompt('Do I enjoy baking?');
+    }
+    let questionFour = prompt('Do I play musical instruments?'); //answer no
+    if (questionFour === null || questionFour === ""){
+        alert ('Enter an answer')
+        let questionFour = prompt('Do I play musical instruments?');
+    }
 
     let count = 0;
     if (ifYes(questionOne)) count ++;
