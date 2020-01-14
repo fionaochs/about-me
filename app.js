071df3278f6fca3d75560efe6433c09370f1ae4e
Fiona Ochs <fiona.oaks@gmail.com>
2020-01-14 09:01:13 -0800

    added add.js

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 import ifYes from "./if-yes.js";
 
-const quizButton = document.getElementById('click', quizButton);
+const quizButton = document.getElementById('quizButton');
 const results = document.getElementById('results');
 
 quizButton.addEventListener('click', () => {
-    
-
     console.log("hit button");
+
+    const name = prompt('What\'s your name?');
+    const confirm = confirm(`Hello ${name}, would you like to take a quiz about Fiona?`)
+
+    if (confirm === false) return;
+
+    const questionOne = prompt('Do I like to travel?'); //answer yes
+    const questionTwo = prompt('Am I a fan of live music?'); //answer yes
+    const questionThree = prompt('Do I enjoy baking?'); //answer yes
+    const questionFour = prompt('Do I play musical instruments?'); //answer no
+
+    let count = 0;
+    if (ifYes(questionOne)) count ++;
+
+    if (ifYes(questionTwo)) count ++;
+
+    if (ifYes(questionThree)) count ++;
+
+    if (!ifYes(questionFour)) count ++;
+    
+    results.textContent = `Wow ${name}, you got ${count} correct`;
 
     document.getElementById('results').style.visibility = "visible";
 })
